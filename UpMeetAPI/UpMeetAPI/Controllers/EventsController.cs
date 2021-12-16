b32d1c68df3069f16b2a079a84a3ed01048515ad
Stephanie <me@stephaniemariewatson.com>
2021-12-15 19:51:59 -0500

    Add Favorites List and Remove

--- UpMeetAPI/UpMeetAPI/Controllers/EventsController.cs
+++ UpMeetAPI/UpMeetAPI/Controllers/EventsController.cs
@@ -25,7 +25,7 @@ namespace UpMeetAPI.Controllers
         [Route("New")]
         public async Task<IActionResult> CreateEvent([Bind("Title,Date,Location,Organization,Details")] Events events)
         {
-            
+
             events.Title = events.Title;
             events.Date = events.Date;
             events.Location = events.Location;
@@ -40,14 +40,7 @@ namespace UpMeetAPI.Controllers
 
         }
 
-        //EventID int Identity(1,1) primary key,
-        //Title nvarchar(255) not null,
-        //Date smalldatetime not null,
-        //Location nvarchar(255) not null,
-        //Organization nvarchar(255) not null,
-        //Details nvarchar(500) not null
 
-        // GET: Events
         [HttpGet]
         [Route("list")]
         public async Task<IActionResult> GetEvents()
@@ -59,134 +52,5 @@ namespace UpMeetAPI.Controllers
         }
 
 
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Events.ToListAsync());
-        }
-
-        // GET: Events/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var events = await _context.Events
-                .FirstOrDefaultAsync(m => m.EventID == id);
-            if (events == null)
-            {
-                return NotFound();
-            }
-
-            return View(events);
-        }
-
-        // GET: Events/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Events/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("EventID,Title,Date,Location,Organization,Details")] Events events)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(events);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(events);
-        }
-
-        // GET: Events/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var events = await _context.Events.FindAsync(id);
-            if (events == null)
-            {
-                return NotFound();
-            }
-            return View(events);
-        }
-
-        // POST: Events/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("EventID,Title,Date,Location,Organization,Details")] Events events)
-        {
-            if (id != events.EventID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(events);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EventsExists(events.EventID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(events);
-        }
-
-        // GET: Events/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var events = await _context.Events
-                .FirstOrDefaultAsync(m => m.EventID == id);
-            if (events == null)
-            {
-                return NotFound();
-            }
-
-            return View(events);
-        }
-
-        // POST: Events/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var events = await _context.Events.FindAsync(id);
-            _context.Events.Remove(events);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool EventsExists(int id)
-        {
-            return _context.Events.Any(e => e.EventID == id);
-        }
     }
 }
